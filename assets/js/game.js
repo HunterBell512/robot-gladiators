fe2922bb3c5273783e30d93ec08ee0c22140aaab
Hunter Bell <bellhunter512@gmail.com>
2022-04-10 23:30:05 -0400

    multiple rounds complete

--- assets/js/game.js
+++ assets/js/game.js
@@ -60,9 +60,15 @@ var fight = function(enemyName) {
 };
 
 for (var i = 0; i < enemyNames.length; i++) {
-    console.log("console is working")
-    enemyHealth = 50;
-    fight(enemyNames[i]);
+    if (playerHealth > 0) {
+        window.alert("Welcome to Robot Gladiators! Round " + (i + 1) );
+        enemyHealth = 50;
+        fight(enemyNames[i]);
+    }
+    else {
+        window.alert("You have lost your robot in battle! Game Over!")
+        break;
+    }
 }
 
 // fight();
